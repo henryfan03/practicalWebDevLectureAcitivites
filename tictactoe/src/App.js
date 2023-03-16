f378d8b5402a0e1c5870ec0b84b81398fa93a493
Henry Fan <henry.fan2313@gmail.com>
2023-03-16 18:48:46 -0400

    added new stuff

---
+++ tictactoe/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import './Solution.js';
+import Game from './Solution.js';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello, World!</h1>
+        <Game/>
+        <Game/>
+        <Game/>
+    </div>
+  );
+}
+
+export default App;
